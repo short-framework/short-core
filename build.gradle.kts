bf6970c7ba0a9f80787220d4f0292ad9bbb8dfd8
Antonio Marques <antoniomarques.exe@gmail.com>
2024-08-11 18:48:19 +0100

    Add linter dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import java.net.URI
 
 plugins {
     kotlin("jvm") version "1.9.20"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
     id("maven-publish")
 }
 
@@ -35,6 +36,11 @@ publishing {
             }
         }
     }
+}
+
+// Optionally configure plugin
+configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+    debug.set(true)
 }
 
 tasks.test {
